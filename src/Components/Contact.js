092f6d9069cb8593235197f55f2284ab40ab3457
camin-mccluskey <c.mccluskey_7@hotmail.co.uk>
2020-06-28 18:07:07 +0100

    Add gsheet integration for contact form submissions

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,6 +1,44 @@
 import React, { Component } from 'react';
+const fetch = require("node-fetch");
+const spreadsheetId = '1nJyDrNkwY-5TMad5yP2dFM6cRL_jL5c-1tt0efrISbs'
 
 class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+      error: false,
+      sent: false
+    };
+  }
+
+  handleChange = (event) => {
+    let nam = event.target.name;
+    let val = event.target.value;
+    this.setState({[nam]: val});
+  }
+
+  submitForm = (e) => {
+    e.preventDefault();
+    const baseUrl = "https://pushtogsheet.herokuapp.com";
+    const query = `valueInputOption=RAW&pizzly_pkey=pope8Qy8qfYyppnHRMgLMpQ8MuEUKDGeyhfGCj`;
+    const url = new URL(`/proxy/google-sheets/spreadsheets/${spreadsheetId}/values/A1:append?${query}`, baseUrl);
+    const { name, email, subject, message} = this.state;
+    const data = [["name", "email", "subject", "message"], [name, email, subject, message]];
+
+    fetch(url.href, {
+      method: "POST",
+      body: JSON.stringify({ values: data }),
+      headers: { 'Pizzly-Auth-Id': '822620f0-b95f-11ea-ae44-0585064b5bfd' }
+    })
+    .then(this.setState({sent: true}))
+    .catch(this.setState({error: true}));
+  }
+
+
   render() {
 
     if(this.props.data){
@@ -26,37 +64,63 @@ class Contact extends Component {
          </div>
          <div className="row">
             <div className="seven columns">
-               <form action="" method="POST" id="contactForm" name="contactForm">
+              {/* https://github.com/dwyl/learn-to-send-email-via-google-script-html-no-server */}
+               <form>
 					      <fieldset> 
                   <div>
 						        <label htmlFor="contactName">Name <span className="required">*</span></label>
-						        <input type="text" defaultValue="" size="35" id="contactName" name="name" onChange={this.handleChange}/>
+                    <input 
+                      type="text" 
+                      defaultValue="" 
+                      size="35" 
+                      id="contactName" 
+                      name="name" 
+                      onChange={this.handleChange}/>
                   </div>
                   <div>
 						        <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-						        <input type="text" defaultValue="" size="35" id="contactEmail" name="email" onChange={this.handleChange}/>
+                    <input 
+                      type="text"
+                      defaultValue=""
+                      size="35"
+                      id="contactEmail"
+                      name="email"
+                      onChange={this.handleChange}
+                    />
                   </div>
                   <div>
 						        <label htmlFor="contactSubject">Subject</label>
-						        <input type="text" defaultValue="" size="35" id="contactSubject" name="subject" onChange={this.handleChange}/>
+                    <input 
+                      type="text"
+                      defaultValue=""
+                      size="35"
+                      id="contactSubject"
+                      name="subject"
+                      onChange={this.handleChange}
+                    />
                   </div>
                   <div>
                      <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                     <textarea cols="50" rows="15" id="contactMessage" name="message"></textarea>
+                     <textarea
+                      cols="50"
+                      rows="15"
+                      id="contactMessage"
+                      name="message"
+                      onChange={this.handleChange}
+                    />
                   </div>
                   <div>
-                     <button className="submit">Submit</button>
-                     <span id="image-loader">
-                        <img alt="" src="images/loader.gif" />
-                     </span>
+                     <button 
+                      style={this.state.sent ? {backgroundColor: "#5cb85c"} : {backgroundColor: "#0275d8"}}
+                      onClick={(e) => {this.submitForm(e)}} 
+                      className="submit">
+                        Submit
+                    </button>
+                    {this.state.sent && <p>Thanks!</p>}
+                    {this.state.error && <p>Error, please try again</p>}
                   </div>
 					</fieldset>
 				   </form>
-
-           <div id="message-warning"> Error boy</div>
-				   <div id="message-success">
-              <i className="fa fa-check"></i>Your message was sent, thank you!<br />
-				   </div>
            </div>
             <aside className="five columns footer-widgets">
                <div className="widget widget_tweets">
