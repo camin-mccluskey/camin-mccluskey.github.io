d94ec98cc18619f2e30a775df025babaab15001b
camin-mccluskey <c.mccluskey_7@hotmail.co.uk>
2020-06-28 12:10:01 +0100

    Updates

---
+++ precache-manifest.d17121bd03724fb9eb6250a310009483.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68e7c569346840cff359021bbca861e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56c1e590823f6f73887c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "20d194997cf2c10a1265",
+    "url": "/static/js/2.70b516f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.70b516f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56c1e590823f6f73887c",
+    "url": "/static/js/main.489268eb.chunk.js"
+  },
+  {
+    "revision": "6f85a85d09db36b59c1d",
+    "url": "/static/js/runtime-main.78807b42.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
