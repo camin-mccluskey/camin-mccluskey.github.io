3f2be4c7629a242e5b69f2d8384c55d604e5bf40
camin-mccluskey <c.mccluskey_7@hotmail.co.uk>
2021-01-26 23:53:45 +0000

    Updates

--- precache-manifest.30c885205ca535586bd7a7a5cab4cc2e.js
+++ precache-manifest.30c885205ca535586bd7a7a5cab4cc2e.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cedaefa558fa23f3e82283f2a0bcf714",
+    "revision": "7f2289ca8b9c2ca9f0552562ab198e67",
     "url": "/index.html"
   },
   {
-    "revision": "a7230293ac30a07d448c",
+    "revision": "114f564aeae7dac5bd39",
     "url": "/static/css/main.5facb584.chunk.css"
   },
   {
-    "revision": "d8543294a9333fdd061b",
-    "url": "/static/js/2.bdbe6bed.chunk.js"
+    "revision": "4711bfb1e29c43d07d62",
+    "url": "/static/js/2.2600b887.chunk.js"
   },
   {
     "revision": "5bba1771fd5b4a2319d369ca22fd2d31",
-    "url": "/static/js/2.bdbe6bed.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.2600b887.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a7230293ac30a07d448c",
-    "url": "/static/js/main.34ca65ce.chunk.js"
+    "revision": "114f564aeae7dac5bd39",
+    "url": "/static/js/main.d610cb37.chunk.js"
   },
   {
     "revision": "6f85a85d09db36b59c1d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9bf4a8a9361b1c1d4e69e35788986c96.js"
+  "/precache-manifest.30c885205ca535586bd7a7a5cab4cc2e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
