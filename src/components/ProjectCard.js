a265846a71d72e71f3e0dde93a4980ac6a652158
camin-mccluskey <c.mccluskey_7@hotmail.co.uk>
2021-10-24 18:14:12 +0100

    Fix hover

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -8,9 +8,9 @@ const ProjectCard = ({ project }) => {
   return (
     <a href={uri} target="_blank" rel="noopener noreferrer">
     <div className="flex flex-col w-full md:min-h-full sm:min-h-full bg-gray-300 dark:bg-gray-900 rounded-lg px-5 py-3 
-                    betterhover:hover:shadow-purple-lg dark:betterhover:hover:shadow-dark-lg
+                    betterhover:hover:shadow-purple-lg betterhover:dark:hover:shadow-dark-lg
                     cursor-pointer 
-                    transition-all ease-in-out duration-300 transform betterhover:hover:-translate-y-1 betterhover:hover:scale-105"
+                    transition-all ease-in-out duration-300 transform betterhover:hover:-translate-y-2 betterhover:hover:scale-105 betterhover:hover:rotate-1"
     >
       <div className="flex-grow h-full">
         <p className="text-2xl text-center dark:text-white darkTrans font-mono">{title}</p>
